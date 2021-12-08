7e18000ba43f1ac4475a44ed01a8a628ef766d0e
yunk-o <67614893+yunk-o@users.noreply.github.com>
2021-12-08 15:14:52 +0900

    Add files via upload

---
+++ precache-manifest.6e3ff00c6cfa64605511e29a22ad1e63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dc6c39c62698442e0c",
+    "url": "/css/app.13afbd01.css"
+  },
+  {
+    "revision": "18404e3d66cf78b43eaf231c553be887",
+    "url": "/follower.json"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bafb07f534ec9228393850c74effdd02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09dc6c39c62698442e0c",
+    "url": "/js/app.049f9cba.js"
+  },
+  {
+    "revision": "ce55518c2a2ec7e85921",
+    "url": "/js/chunk-vendors.311ee05c.js"
+  },
+  {
+    "revision": "7a48e533dbca2832e097778c0f48b6a6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
